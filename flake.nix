46a40923a7edf778f3454b5e4d640d6e2fefc743
DreamingCodes <me@dreaming.codes>
2024-09-27 17:37:33 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Iced example";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, ...}:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = {self', pkgs, system, ...}:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          runtimeDeps = with pkgs; [
+            wayland libxkbcommon libGL
+          ];
+        in {
+          devShells.default = pkgs.mkShell rec {
+            buildInputs = with pkgs; [
+              pkg-config
+            ] ++ runtimeDeps;
+            LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath buildInputs}";
+          };
+        };
+    };
+}
